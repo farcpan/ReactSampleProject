287acf323e08a819dc49687169896847cb545bb8
myname <mymail>
2021-06-26 01:37:41 +0900

    testing font

--- packages/webapp/src/App.tsx
+++ packages/webapp/src/App.tsx
@@ -2,15 +2,31 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import { Typography } from '@material-ui/core';
+import { makeStyles } from '@material-ui/core/styles';
+
 // components
 import * as ApiPackage from 'api';
 import { TestComponent } from 'components';
 import { CheckboxTreeView, createTestTreeViewData, TreeViewData } from "./components/checkboxtreeview";
 
-function App() {
+const useStyles = makeStyles((theme) =>({
+  typo: {
+    fontFamily: "'Noto Sans JP', 'Noto Sans KR', 'Noto Sans TC', sans-serif;",
+    fontWeight: 700,
+  },
+  typo_tc: {
+    fontFamily: "'Noto Sans TC', sans-serif;",
+    fontWeight: 700,
+  }
+}));
+
+const App = () => {
   const value = ApiPackage.test();
 
   const data: TreeViewData[] = createTestTreeViewData();
+
+  const styles = useStyles();
 
   return (
     <div className="App">
@@ -34,6 +50,13 @@ function App() {
           Learn React
         </a>
       </header>
+
+      {/* font test area */}
+      <div style={{ width: "100%", backgroundColor: "#ffddff", padding: "20px"}}>
+        <Typography className={styles.typo}>テスト文章をここに書きます。フォントを比較します。Here is the font test space.</Typography>
+        <Typography className={styles.typo_tc}>テスト文章をここに書きます。フォントを比較します。Here is the font test space.</Typography>
+        <Typography>テスト文章をここに書きます。フォントを比較します。Here is the font test space.</Typography>
+      </div>
     </div>
   );
 }
